46b00b01a5d456948f6bc73ec29ec0300e0adf5e
Leon Ding <deen.job@qq.com>
2025-02-12 22:45:44 +0800

    fix: indexs inode number murmurhash3 algorihm.

--- vfs/lfs.go
+++ vfs/lfs.go
@@ -6,7 +6,6 @@ import (
 	"errors"
 	"fmt"
 	"hash/crc32"
-	"hash/fnv"
 	"io"
 	"io/fs"
 	"os"
@@ -19,6 +18,7 @@ import (
 
 	"github.com/auula/wiredkv/clog"
 	"github.com/auula/wiredkv/utils"
+	"github.com/spaolacci/murmur3"
 )
 
 const RWCA = os.O_RDWR | os.O_CREATE | os.O_APPEND
@@ -220,9 +220,7 @@ func (lfs *LogStructuredFS) KeysCount() int {
 }
 
 func InodeNum(key string) uint64 {
-	h := fnv.New64a()
-	h.Write([]byte(key))
-	return h.Sum64()
+	return murmur3.Sum64([]byte(key))
 }
 
 func (lfs *LogStructuredFS) changeRegions() error {
